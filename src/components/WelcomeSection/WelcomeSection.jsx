67dc7268916ec2f22d47bfdae32eeaf2cf5da7f0
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-09-16 14:28:04 +0300

    refactor (styles): created sidebar for public pages

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -1,26 +1,29 @@
 import {Link} from "react-router-dom";
 
 import Logo from "../Logo/Logo";
+import HomeAuthSidebar from "../HomeAuthSidebar/HomeAuthSidebar";
 
 import css from "./WelcomeSection.module.css";
 
 const WelcomeSection = () => {
   return (
-    <div className={css.wrapper}>
-      <Logo />
-      <div className={css.context}>
-        <h2 className={css.subtitle}>Record daily water intake and track</h2>
-        <h1 className={css.title}>Water consumption tracker</h1>
-        <span className={css.wrapper_links}>
-          <Link to="/signup" className={css.link_try_tracker}>
-            Try tracker
-          </Link>
-          <Link to="/signin" className={css.link_sign_in_class}>
-            Sign In
-          </Link>
-        </span>
+    <HomeAuthSidebar>
+      <div className={css.wrapper}>
+        <Logo />
+        <div className={css.context}>
+          <h2 className={css.subtitle}>Record daily water intake and track</h2>
+          <h1 className={css.title}>Water consumption tracker</h1>
+          <span className={css.wrapper_links}>
+            <Link to="/signup" className={css.link_try_tracker}>
+              Try tracker
+            </Link>
+            <Link to="/signin" className={css.link_sign_in_class}>
+              Sign In
+            </Link>
+          </span>
+        </div>
       </div>
-    </div>
+    </HomeAuthSidebar>
   );
 };
 export default WelcomeSection;
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,18 @@
 import MediaQuery from "react-responsive";
 import AdvantagesSection from "../../components/AdvantagesSection/AdvantagesSection";
 import WrapperForPublicRoutes from "../../components/WrapperForPublicRoutes/WrapperForPublicRoutes";
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import Logo from "../../components/Logo/Logo";
+import HomeAuthSidebar from "../../components/HomeAuthSidebar/HomeAuthSidebar";
 
 const Register = () => {
   return (
     <WrapperForPublicRoutes>
-      Register
+      <HomeAuthSidebar>
+        <Logo />
+        <SignUpForm />
+      </HomeAuthSidebar>
+
       <MediaQuery minWidth={1440}>
         <AdvantagesSection />
       </MediaQuery>
