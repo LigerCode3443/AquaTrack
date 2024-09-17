232190949245ab644dedabd7d233c579068748fd
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-09-17 19:54:10 +0300

    fix: rename fields

--- src/helpers/schemaValidateAuth.js
+++ src/helpers/schemaValidateAuth.js
@@ -1,16 +1,16 @@
 import * as Yup from "yup";
 
 export const schemaRegister = Yup.object({
-  email: Yup.string().email("Invalid email address").required("Email is required"),
-  password: Yup.string()
+  userEmail: Yup.string().email("Invalid email address").required("Email is required"),
+  userPassword: Yup.string()
     .min(6, "Password must be at least 6 characters")
     .required("Password is required"),
   repeatPassword: Yup.string()
-    .oneOf([Yup.ref("password"), null], "Passwords do not match!")
+    .oneOf([Yup.ref("userPassword"), null], "Passwords do not match!")
     .required("Repeat password is required"),
 });
 
 export const schemaLogin = Yup.object({
-  email: Yup.string().email("Invalid email address").required("Email is required"),
-  password: Yup.string().required("Password is required"),
+  userEmail: Yup.string().email("Invalid email address").required("Email is required"),
+  userPassword: Yup.string().required("Password is required"),
 });
