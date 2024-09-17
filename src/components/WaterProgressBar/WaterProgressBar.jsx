4e345d278d15ac969ac74c04a25c6a6df5a60b1d
Yaroslav <skylandtat@ukr.net>
2024-09-17 11:49:11 +0200

    Add/edit WaterProgressBar

--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -1,11 +1,34 @@
 import css from "./WaterProgressBar.module.css";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
-const WaterProgressBar = ({ progress }) => {
+const WaterProgressBar = ({ value, setValue }) => {
   return (
-    <div className={css.WaterProgressBar}>
-      <label>Today</label>
-      <input type="range" min="0" max="100" value={progress} readOnly />
-      <p>{progress}%</p>
+    <div className={css.waterProgressBar}>
+      <h3>Today</h3>
+      <Slider
+        min={0}
+        max={100}
+        value={value}
+        onChange={setValue}
+        trackStyle={{ backgroundColor: "#9BE1A0", height: 6 }}
+        handleStyle={{
+          borderColor: "#9BE1A0",
+          height: 12,
+          width: 12,
+          marginLeft: 3,
+          marginTop: -3,
+          backgroundColor: "#fff",
+          borderWidth: 1,
+        }}
+        railStyle={{ backgroundColor: "#FFFFFF", height: 6 }}
+        disabled={true}
+      />
+      <div className={css.waterProcent}>
+        <span>0%</span>
+        <span>50%</span>
+        <span>100%</span>
+      </div>
     </div>
   );
 };
