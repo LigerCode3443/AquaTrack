f69238e7c64280f7bfe3afaa7504a08d28cfbbee
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-09-17 20:39:53 +0300

    feat: proper password

--- src/helpers/schemaValidateAuth.js
+++ src/helpers/schemaValidateAuth.js
@@ -4,6 +4,11 @@ export const schemaRegister = Yup.object({
   userEmail: Yup.string().email("Invalid email address").required("Email is required"),
   userPassword: Yup.string()
     .min(6, "Password must be at least 6 characters")
+    .max(12, "Password must not exceed 12 characters")
+    .matches(
+      /^(?=.*[0-9])(?=.*[a-z])(?!.* ).{6,12}$/,
+      "Password must have a number, a letter, and no spaces"
+    )
     .required("Password is required"),
   repeatPassword: Yup.string()
     .oneOf([Yup.ref("userPassword"), null], "Passwords do not match!")
