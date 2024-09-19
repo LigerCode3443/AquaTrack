f8cc052a8adc9a2659546e276343af316fcb9831
LieraBerh <lierrastern@gmail.com>
2024-09-19 12:20:02 +0200

    add media queries

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import PublicRoute from "./routes/PublicRoute";
-import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
-import PrivateRoute from "./routes/PrivateRoute";
-import Tracker from "./pages/Trecker/Tracker";
+// import PublicRoute from "./routes/PublicRoute";
+// import Home from "./pages/Home/Home";
+// import Login from "./pages/Login/Login";
+// import Register from "./pages/Register/Register";
+// import PrivateRoute from "./routes/PrivateRoute";
+// import Tracker from "./pages/Trecker/Tracker";
 import Container from "./components/Container/Container";
 import { useDispatch, useSelector } from "react-redux";
 import { selectIsRefresh } from "./redux/auth/selectors";
 import { useEffect } from "react";
 import { refreshThunk } from "./redux/auth/operations";
+import AddWaterForm from "./components/AddWaterForm/AddWaterForm";
 
 function App() {
   const dispatch = useDispatch();
@@ -24,7 +25,8 @@ function App() {
     <p>Loading</p>
   ) : (
     <Container>
-      <Routes>
+      <AddWaterForm />
+      {/* <Routes>
         <Route
           path="/"
           element={
@@ -57,7 +59,7 @@ function App() {
             </PrivateRoute>
           }
         />
-      </Routes>
+      </Routes> */}
     </Container>
   );
 }
