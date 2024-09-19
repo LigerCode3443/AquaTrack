5bc06d13ac3430c8d3ef1b9b26ced5b7cb8a3290
Yaroslav <skylandtat@ukr.net>
2024-09-19 10:13:02 +0200

    mini fix

--- src/components/WaterDailyNorma/WaterDailyNorma.jsx
+++ src/components/WaterDailyNorma/WaterDailyNorma.jsx
@@ -1,9 +1,14 @@
 import css from "./WaterDailyNorma.module.css";
+// import { useSelector } from "react-redux";
 
-const WaterDailyNorma = ({ dailyNorma }) => {
+const WaterDailyNorma = () => {
+  // const dailyNorma = useSelector();
+
   return (
     <div className={css.waterDailyNorma}>
-      <h4>{dailyNorma} L</h4>
+      {/* <h4>{dailyNorma ? dailyNorma : 0} L</h4>
+       */}
+      <h4>0 L</h4>
       <p>My daily norma</p>
     </div>
   );
