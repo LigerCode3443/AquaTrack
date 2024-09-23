c4344efa24bc56157e7c89080640b88a17788dcc
LieraBerh <lierrastern@gmail.com>
2024-09-23 09:00:11 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-// import { Route, Routes } from "react-router-dom";
-// import { Toaster } from "react-hot-toast";
+import { Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import "./App.css";
-// import PublicRoute from "./routes/PublicRoute";
-// import Home from "./pages/Home/Home";
-// import Login from "./pages/Login/Login";
-// import Register from "./pages/Register/Register";
-// import PrivateRoute from "./routes/PrivateRoute";
-// import Tracker from "./pages/Tracker/Tracker";
+import PublicRoute from "./routes/PublicRoute";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import PrivateRoute from "./routes/PrivateRoute";
+import Tracker from "./pages/Tracker/Tracker";
 import Container from "./components/Container/Container";
 import { useDispatch, useSelector } from "react-redux";
 import { selectIsRefresh } from "./redux/auth/selectors";
 import { useEffect } from "react";
 import { refreshThunk } from "./redux/auth/operations";
-import EditWaterForm from "./components/EditWaterForm/EditWaterForm";
 
 function App() {
   const dispatch = useDispatch();
@@ -26,8 +25,7 @@ function App() {
     <p>Loading</p>
   ) : (
     <Container>
-      <EditWaterForm />
-      {/* <Routes>
+      <Routes>
         <Route
           path="/"
           element={
@@ -61,7 +59,7 @@ function App() {
           }
         />
       </Routes>
-      <Toaster position="top-center" reverseOrder={false} /> */}
+      <Toaster position="top-center" reverseOrder={false} />
     </Container>
   );
 }
