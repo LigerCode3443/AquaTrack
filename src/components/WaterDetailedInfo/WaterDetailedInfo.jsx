9b109f7249d23c58317b3ee94b2bf4711ec81dbd
LigerCode3443 <161658640+LigerCode3443@users.noreply.github.com>
2024-09-23 21:01:23 +0300

    fix wrapper

--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -1,10 +1,11 @@
 import DailyInfo from "../DailyInfo/DailyInfo";
 import UserBar from "../UserBar/UserBar";
 import Calendar from "../Calendar/Calendar";
+import s from "./WaterDetailedInfo.module.css";
 
 const WaterDetailedInfo = ({ date, waterData }) => {
   return (
-    <div>
+    <div className={s.wrapper}>
       <UserBar />
       <DailyInfo date={date} waterData={waterData} />
       <Calendar />
