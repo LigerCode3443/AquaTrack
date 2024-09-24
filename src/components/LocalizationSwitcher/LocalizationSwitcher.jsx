eb10eedfcaf5c0a60388545cb02ff6b95da011b3
Oleksandr Zhynkin <godinsighted@gmail.com>
2024-09-24 23:39:43 +0300

    style switcher

--- src/components/LocalizationSwitcher/LocalizationSwitcher.jsx
+++ src/components/LocalizationSwitcher/LocalizationSwitcher.jsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 
-import s from "../LocalizationSwitcher/LocalizationSwitcher.module.css";
+import css from "../LocalizationSwitcher/LocalizationSwitcher.module.css";
 
 export const LocalizationSwitcher = () => {
   const languages = {
@@ -15,16 +15,16 @@ export const LocalizationSwitcher = () => {
   };
 
   return (
-    <div>
+    <div className={css.wrapper}>
       {Object.keys(languages).map((lng) => (
         <button
           key={lng}
           onClick={() => handleChange(lng)}
-          className={`${s.button} ${
-            i18n.resolvedLanguage === lng ? s.active : ""
+          className={`${css.button} ${css[lng]} ${
+            i18n.resolvedLanguage === lng ? css.active : ""
           }`}
         >
-          {languages[lng].name}
+          {/* {languages[lng].name} */}
         </button>
       ))}
     </div>
