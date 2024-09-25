05bd9f9a21390d6909bbf6e539ff56cff4bd8590
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-09-25 14:52:59 +0300

    feat: lazyload

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
+import {Suspense, lazy, useEffect} from "react";
 import {Route, Routes} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
 import PublicRoute from "./routes/PublicRoute";
+import PrivateRoute from "./routes/PrivateRoute";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
-import PrivateRoute from "./routes/PrivateRoute";
-import Tracker from "./pages/Tracker/Tracker";
-import {useDispatch, useSelector} from "react-redux";
-import {selectIsRefresh} from "./redux/auth/selectors";
-import {useEffect} from "react";
-import {refreshThunk} from "./redux/auth/operations";
-import ChangePassword from "./pages/ChangePassword/ChangePassword";
-import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import SharedLayout from "./components/SharedLayout/SharedLayout";
+import {selectIsRefresh} from "./redux/auth/selectors";
+import {refreshThunk} from "./redux/auth/operations";
+import Loader from "./components/Loader/Loader";
+
+const Register = lazy(() => import("./pages/Register/Register"));
+const Tracker = lazy(() => import("./pages/Tracker/Tracker"));
+const ChangePassword = lazy(() => import("./pages/ChangePassword/ChangePassword"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound/PageNotFound"));
 
 function App() {
   const dispatch = useDispatch();
@@ -22,54 +24,56 @@ function App() {
   }, [dispatch]);
 
   return isRefreshing ? (
-    <p>Loading</p>
+    <Loader />
   ) : (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route
-          index
-          element={
-            <PublicRoute>
-              <Home />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/signup"
-          element={
-            <PublicRoute>
-              <Register />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/signin"
-          element={
-            <PublicRoute>
-              <Login />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/password-recovery/:verificationToken"
-          element={
-            <PublicRoute>
-              <ChangePassword />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/tracker"
-          element={
-            <PrivateRoute>
-              <Tracker />
-            </PrivateRoute>
-          }
-        />
-      </Route>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route
+            index
+            element={
+              <PublicRoute>
+                <Home />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/signin"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/password-recovery/:verificationToken"
+            element={
+              <PublicRoute>
+                <ChangePassword />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/tracker"
+            element={
+              <PrivateRoute>
+                <Tracker />
+              </PrivateRoute>
+            }
+          />
+        </Route>
 
-      <Route path="/*" element={<PageNotFound />} />
-    </Routes>
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </Suspense>
   );
 }
 
