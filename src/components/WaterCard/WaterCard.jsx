1c80b2b43d468e2c4c7fcd5a56daea01ddedd543
Cheastear <56431973+Cheastear@users.noreply.github.com>
2024-09-26 10:38:29 +0300

    fixes
    + mobile view WaterCard
    + change view from ml to L

--- src/components/WaterCard/WaterCard.jsx
+++ src/components/WaterCard/WaterCard.jsx
@@ -26,7 +26,7 @@ const WaterCard = ({ quantity, time, onEdit, onDelete }) => {
       </MediaQuery>
 
       <div className={s.waterInfo}>
-        <p className={s.quantity}>{quantity} ml</p>
+        <p className={s.quantity}>{(quantity / 1000).toFixed(2)} L</p>
         <p className={s.time}>{formattedTime}</p>
       </div>
 
